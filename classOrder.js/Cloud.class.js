695eb477dfd5d14ed91b71b178d0eb8295446cb0
Salih Derin <derin.salih@gmx.de>
2026-06-30 11:26:16 +0200

    created clouds object

---
+++ classOrder.js/Cloud.class.js
@@ -0,0 +1,13 @@
+import { MovableObject } from "./movable-object.class.js";
+
+export class Cloud extends MovableObject{
+y = 20;
+height = 250;
+width = 500;
+
+    constructor() {
+        super();
+        this.loadImage('img/5_background/layers/4_clouds/1.png');
+        this.x = Math.random() * 500;
+}
+}
--- classOrder.js/world.class.js
+++ classOrder.js/world.class.js
@@ -1,5 +1,6 @@
 import { Character } from "./Character.class.js";
 import { Chicken } from "./chicken.class.js";
+import { Cloud } from "./Cloud.class.js";
 
 
 export class World {
@@ -8,6 +9,9 @@ export class World {
     new Chicken(),
     new Chicken(),
     new Chicken()
+    ];
+    clouds = [
+        new Cloud()
     ];
 
     canvas;
@@ -25,6 +29,10 @@ export class World {
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
         this.enemies.forEach(enemy => {
             this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+        });
+
+        this.clouds.forEach(cloud => {
+            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
         });
 
         // draw() is called repeatedly
