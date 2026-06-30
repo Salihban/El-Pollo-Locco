bc52477e04e33f80c1f724b064b56e3416ea6373
Salih Derin <derin.salih@gmx.de>
2026-06-30 13:54:49 +0200

    create background Imges

---
+++ classOrder.js/background-Object.class.js
@@ -0,0 +1,14 @@
+import { MovableObject } from "./movable-object.class.js";
+
+
+export class BackgroundObject extends MovableObject {
+
+    width = 720;
+    height = 400;
+    constructor(imagePath, x, y) {
+        super().loadImage(imagePath);
+        this.x = x;
+        this.y = 480 - this.height;
+    }
+
+}
