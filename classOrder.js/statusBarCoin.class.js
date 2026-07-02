d3681a0500080b53ffbd95c99997eec76b63e6d3
Salih Derin <derin.salih@gmx.de>
2026-07-02 19:28:36 +0200

    statusCoinBar fixed to resolveImageIndex()

--- classOrder.js/statusBarCoin.class.js
+++ classOrder.js/statusBarCoin.class.js
@@ -12,7 +12,7 @@ export class StatusBarCoin extends DrawableObject {
         'img/7_statusbars/1_statusbar/1_statusbar_coin/green/100.png',
     ];
 
-    percentage =0;
+    percentage = 0;
 
     constructor() {
         super();
@@ -21,7 +21,7 @@ export class StatusBarCoin extends DrawableObject {
         this.y = 40;
         this.width = 200;
         this.height = 60;
-        this.setPercentage(0);
+        this.setPercentage (0);
     }
 
     // setPercentage(50);
@@ -32,15 +32,15 @@ export class StatusBarCoin extends DrawableObject {
     }
 
     resolveImageIndex() {
-        if (this.percentage == 100) {
+        if (this.percentage >= 100) {
             return 5;
-        } else if (this.percentage > 80) {
+        } else if (this.percentage >= 80) {
             return 4;
-        } else if (this.percentage > 60) {
+        } else if (this.percentage >= 60) {
             return 3;
-        } else if (this.percentage > 40) {
+        } else if (this.percentage >= 40) {
             return 2;
-        } else if (this.percentage > 20) {
+        } else if (this.percentage >= 20) {
             return 1;
         } else {
             return 0;
